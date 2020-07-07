b061f02a27f9b11af61ec32a4b609bff3e483094
Praveena Sundararajan <50090917+Praveena2k@users.noreply.github.com>
2020-07-06 20:29:56 -0700

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SwapNumbers
+{
+public static void main(String args[])
+{
+int z, y, temp;
+System.out.println("Enter z and y");
+Scanner sct = new Scanner(System.in);   //User inputs two numbers
+z = 10;   //User inputs two numbers
+y = 5;
+System.out.println("Before Swapping\nz = "+z+"\ny = "+y);
+temp = z;   //Swapping is done
+z = y;
+y = temp;
+System.out.println("After Swapping\nz = "+z+"\ny = "+y);
+}
+}
