b59ab4d06b1cba14e1af976d9678f0df93bf8cb6
Praveena Sundararajan <50090917+Praveena2k@users.noreply.github.com>
2020-07-08 10:43:49 +0530

    swap

--- sample.java
+++ sample.java
@@ -7,7 +7,7 @@ int z, y, temp;
 System.out.println("Enter z and y");
 Scanner sct = new Scanner(System.in);   //User inputs two numbers
 z = 20;   //User inputs two numbers.
-y = 6;
+y = 16;
 System.out.println("Before Swapping\nz = "+z+"\ny = "+y);
 temp = z;   //Swapping.
 z = y;
